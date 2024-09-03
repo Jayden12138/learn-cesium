8b25a6828c7a698a65c6c665bf55d9ac18d280b3
dengjj <18993882884@163.com>
2024-09-03 17:23:54 +0800

    feat: add cesium

---
+++ src/components/Map/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="map_container">
+        <div id="cesium_container"></div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import { Terrain, Viewer, Ion } from 'cesium'
+import 'cesium/Build/Cesium/Widgets/widgets.css'
+
+onMounted(() => {
+    Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_ACCESS_TOKEN
+
+    new Viewer('cesium_container', {
+        terrain: Terrain.fromWorldTerrain()
+    })
+})
+</script>
+
+<style scoped>
+#cesium_container {
+    background: gray;
+    height: 100vh;
+    width: 100%;
+}
+</style>
